47ad65756d0f8b14a6bf40466f50c20a29dbfb12
idris5280 <96846315+idris5280@users.noreply.github.com>
2021-12-29 17:22:53 -0700

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,11 +7,11 @@ module.exports = {
     // You can overwrite values here that are used for the SEO component
     // You can also add new values here to query them like usual
     // See all options: https://github.com/LekoArts/gatsby-themes/blob/main/themes/gatsby-theme-cara/gatsby-config.js
-    siteTitle: `Cara`,
-    siteTitleAlt: `Cara - Gatsby Starter Portfolio`,
-    siteHeadline: `Cara - Gatsby Theme from @lekoarts`,
-    siteUrl: `https://cara.lekoarts.de`,
-    siteDescription: `Playful and Colorful One-Page portfolio featuring Parallax effects and animations`,
+    siteTitle: `Sollers Group`,
+    siteTitleAlt: `The Sollers Group`,
+    siteHeadline: `Tech & IT? We make it easy.`,
+    siteUrl: `https://sollersgroup.com`,
+    siteDescription: `Our goal is to provide training, job placement, and mentorship to help pave the way for professionals seeking career growth.`,
     siteLanguage: `en`,
     siteImage: `/banner.jpg`,
     author: `@lekoarts_de`,
